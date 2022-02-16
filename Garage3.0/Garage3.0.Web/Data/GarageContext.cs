ebc302ceb9d1683025dace91aa62a9598b369440
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-16 11:32:06 +0100

    Initiate Solution and Project

---
+++ Garage3.0/Garage3.0.Web/Data/GarageContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Garage3._0.Web.Data;
+
+public class GarageContext : DbContext
+{
+    public GarageContext(DbContextOptions<GarageContext> options)
+        : base(options)
+    {
+
+    }
+}
---
+++ Garage3.0/Garage3.0.Web/Garagemapper/GarageMappings.cs
@@ -0,0 +1,7 @@
+﻿using AutoMapper;
+
+namespace Garage3._0.Web.Garagemapper;
+
+public class GarageMappings : Profile
+{
+}
