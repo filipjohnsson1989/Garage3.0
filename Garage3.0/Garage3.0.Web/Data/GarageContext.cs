37e671c6ab084a6f78e6dbcfd18d45ba0f8bf90b
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-17 09:58:22 +0100

    Rename a bit VehicleType Entity and Add Wheels property

--- Garage3.0/Garage3.0.Web/Data/GarageContext.cs
+++ Garage3.0/Garage3.0.Web/Data/GarageContext.cs
@@ -13,5 +13,5 @@ public class GarageContext : DbContext
     {
 
     }
-    public DbSet<Garage3._0.Web.Models.Entities.VehicleType> VehicleType { get; set; }
+    public DbSet<Garage3._0.Web.Models.Entities.VehicleTypeEntity> VehicleTypeEntity { get; set; }
 }
---
+++ Garage3.0/Garage3.0.Web/Models/Entities/VehicleTypeEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage3._0.Web.Models.Entities
+{
+    public class VehicleTypeEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Wheels { get; set; }
+
+    }
+}
