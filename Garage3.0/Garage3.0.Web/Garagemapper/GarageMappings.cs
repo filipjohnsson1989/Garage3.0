e5b0500a8eedee265e737f493ec91d0a6e95923a
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-02-18 10:57:25 +0100

    Ändrat namn på view model Funktion i Controller - funkar inte

--- Garage3.0/Garage3.0.Web/Garagemapper/GarageMappings.cs
+++ Garage3.0/Garage3.0.Web/Garagemapper/GarageMappings.cs
@@ -1,7 +1,13 @@
 ﻿using AutoMapper;
+using Garage3._0.Web.Models.Entities;
+using Garage3._0.Web.Models.ViewModels;
 
 namespace Garage3._0.Web.Garagemapper;
 
 public class GarageMappings : Profile
 {
+    public GarageMappings()
+    {
+        CreateMap<MemberEntity, MemberCreateViewModel>(); 
+    }
 }
