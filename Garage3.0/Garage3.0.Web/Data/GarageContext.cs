42c784d018e9b4d91f2ce73019dce63d74a5d6f5
shadowsental <mattias.celius@gmail.com>
2022-02-18 13:29:18 +0100

    parkingspots

--- Garage3.0/Garage3.0.Web/Data/GarageContext.cs
+++ Garage3.0/Garage3.0.Web/Data/GarageContext.cs
@@ -15,6 +15,7 @@ public class GarageContext : DbContext
     {
 
     }
-    public DbSet<Garage3._0.Web.Models.Entities.VehicleType> VehicleType { get; set; }
+    public DbSet<Garage3._0.Web.Models.Entities.VehicleTypeEntity> VehicleType { get; set; }
     public DbSet<Garage3._0.Web.Models.Entities.MemberEntity> MemberEntity { get; set; }
+    public DbSet<Garage3._0.Web.Models.Entities.ParkingSpot> ParkingSpot { get; set; }
 }
--- Garage3.0/Garage3.0.Web/Data/SeedData.cs
+++ Garage3.0/Garage3.0.Web/Data/SeedData.cs
@@ -16,6 +16,9 @@ namespace Garage3._0.Web.Data
             var vehicleTypes = GetVehicleTypes();
             await db.AddRangeAsync(vehicleTypes);
 
+            var parkingSpots = GetParkingSpots();
+            await db.AddRangeAsync(parkingSpots);
+
             await db.SaveChangesAsync();
         }
 
@@ -30,11 +33,44 @@ namespace Garage3._0.Web.Data
             vehicleTypes.Add(new VehicleTypeEntity { Title = "Airplane", Wheels = 3 });
 
 
-
-
-
-
             return vehicleTypes;
+        }
+
+        private static IEnumerable<ParkingSpot> GetParkingSpots()
+        {
+            var parkingSpots = new List<ParkingSpot>();
+
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A1" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A2" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A3" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A4" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A5" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A6" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A7" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A8" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "A9" });
+
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B1" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B2" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B3" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B4" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B5" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B6" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B7" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B8" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "B9" });
+
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C1" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C2" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C3" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C4" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C5" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C6" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C7" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C8" });
+            parkingSpots.Add(new ParkingSpot { SpotNumber = "C9" });
+
+            return parkingSpots;
         }
     }
 }
