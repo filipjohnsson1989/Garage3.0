1efa9e9c1ad2b44cb998aeb6c387b3053e066528
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-02-18 18:45:06 +0100

    Validations inte klar

--- Garage3.0/Garage3.0.Web/Validations/CheckAgeAttribute.cs
+++ Garage3.0/Garage3.0.Web/Validations/CheckAgeAttribute.cs
@@ -1,25 +1,50 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Garage3._0.Web.Validations
 {
     public class CheckAgeAttribute : ValidationAttribute
     {
-        private readonly int nr;
-        public CheckAgeAttribute(int nr)
+        private readonly string min;
+        public CheckAgeAttribute(string min)
         {
-            this.nr = nr;
+            this.min = min;
         }
 
         public override bool IsValid(object? value)
         {
-            if (value is int input)
+            if (value is string personNr)
             {
-                var num = input;
+                //Personnummer.Valid("191212121212");     // => True
+                //Personnummer.Valid("121212+1212")       // => True
+                //Personnummer.Valid("20121212-1212")     // => True
 
-                return num >= nr;
+                string[] dateValues = { "191212121212", "121212+1212",
+                              "20121212-1212"};
+                string pattern = "MM-dd-yy";
+                DateTime parsedDate;
+                foreach (var dateValue in dateValues)
+                {
+                    if (DateTime.TryParseExact(dateValue, pattern, null,
+                                              DateTimeStyles.None, out parsedDate))
+                        Console.WriteLine("Converted '{0}' to {1:d}.",
+                                          dateValue, parsedDate);
+                    else
+                        Console.WriteLine("Unable to convert '{0}' to a date and time.",
+                                          dateValue);
+                }
+
+
+                //var date = personNr.Split('-');
+                //var age = DateTime.TryParseExact(date);
+
+
+
             }
             return false;
         }
 
     }
 }
+
+
