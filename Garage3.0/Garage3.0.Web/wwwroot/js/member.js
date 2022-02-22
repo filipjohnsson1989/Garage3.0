3567f3e782c39e5ae1b63160ade12560416083a9
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-02-22 02:12:27 +0100

    Cleaning and Renaming

--- Garage3.0/Garage3.0.Web/wwwroot/js/member.js
+++ Garage3.0/Garage3.0.Web/wwwroot/js/member.js
@@ -13,21 +13,18 @@
         },
         success: function (data) {
             response($.map(data.d, function (item) {
-                console.log(item);
                 return {
-                    label: item.name, // Set the display lebel for the searched list of company names which we're getting from server side coding.
+                    label: item.name, // Set the display lebel for the searched list of member names which we're getting from server side coding.
                     value: item.id // Set the raw value of each shown items.
                 }
             }));
         },
         select: function (event, ui) {
-            //console.log(ui.item);
             setMember(ui.item.id, ui.item.name);
             return false;
         }
     })
         .autocomplete("instance")._renderItem = function (ul, item) {
-            item.label = item.name;
             return $("<li>")
                 .append("<div>Name:" + item.name + " PrN:" + item.personNr + "</div>")
                 .appendTo(ul);
